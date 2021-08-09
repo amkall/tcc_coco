8e0670642eeaaa8293c6d2a167bddee8a4c1f185
bruno araujo muniz <brunogt03826@gmail.com>
2021-08-09 17:16:43 -0300

    atualização da função objetivo

--- code-experiments/build/matlab/myobjfunc.m
+++ code-experiments/build/matlab/myobjfunc.m
@@ -1,6 +1,12 @@
-function F = myobjfunc(x)
-x1=x(1);
-x2=x(2);
-x3=x(3);
-F=(x3 + 2)*x2*x1^2;
+function F = myobjfunc(x, dimension)
+    fit = 0;
+    for j = 1:dimension
+        fit = fit + (j+1)*(x(j)*x(j));
+    end
+%x1=x(1);
+%x2=x(2);
+%x3=x(3);
+%F=(x3 + 2)*x2*x1^2;
+
+    F = fit;
 end
